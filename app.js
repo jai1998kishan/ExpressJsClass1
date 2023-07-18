b3f989813affeaaabacb9d03624da52c56b8d857
jai1998kishan <jaikishangupta754@gmail.com>
2023-07-18 21:22:44 +0530

    creatingDifferentRouters

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const bodyParser=require('body-parser');
 
 const app=express();
 
+const adminRouters=require('./routes/admin');
+
+const shopRouters=require('./routes/shop');
+
+
+
 //use allow as to add new middleware function
 // app.use((req,res,next)=>{
 //     console.log('In the middleware')
@@ -18,25 +24,41 @@ const app=express();
 app.use(bodyParser.urlencoded({extended: false}));
 
 
-app.use('/add-product',(req,res,next)=>{
-    // console.log('In another middleware');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>');
-}); 
+//filtering the paths
+app.use('/admin',adminRouters);
 
-app.post('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
+app.use(shopRouters);
+
+
+// app.use('/add-product',(req,res,next)=>{
+//     // console.log('In another middleware');
+//     res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>');
+// }); 
+
+// app.post('/product',(req,res,next)=>{
+//     console.log(req.body);
+//     res.redirect('/');
+// })
+
+// app.use('/',(req,res,next)=>{
+//     // console.log('In another middleware');
+//     // res.send('<h1>Hello from express</h1>');
+//     res.send('<h1>Hello2 from express</h1>');
+//     // res.send('Hello from express with normal text');
+//     // res.send({key1:'value'});
+// });  
+
+
+
+
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page is not Found</h1>');
 })
 
-app.use('/',(req,res,next)=>{
-    // console.log('In another middleware');
-    // res.send('<h1>Hello from express</h1>');
-    res.send('<h1>Hello2 from express</h1>');
-    // res.send('Hello from express with normal text');
-    // res.send({key1:'value'});
-});  
+
+
+
 
 // const server=http.createServer(app);
-
 // server.listen(3000);
 app.listen(3000);
